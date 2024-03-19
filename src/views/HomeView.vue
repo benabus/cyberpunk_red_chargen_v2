ff2689d97df86cf1c9841701b7e28bbba0f4fd93
Ben Serrette <ben.serrette@outlook.com>
2024-03-19 00:31:38 -0400

    Translates character sheet to classes.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,242 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+import type { EnumDeclaration } from 'typescript';
+import {ref} from 'vue';
+
+enum Role {
+  Solo = "Solo",
+  Fixer = "Fixer",
+  Netrunner = "Netrunner",
+  Civilian = "Civilian",
+  Rockerboy = "Rockerboy",
+  Tech = "Tech",
+  Medtech = "Medtech",
+  Media = "Media",
+  Exec = "Exec",
+  Lawman = "Lawman",
+  Nomad = "Nomad",
+}
+
+enum Stat {
+  INT = "INT",
+  REF = "REF",
+  DEX = "DEX",
+  TECH = "TECH",
+  COOL = "COOL",
+  WILL = "WILL",
+  LUCK = "LUCK",
+  MOVE = "MOVE",
+  BODY = "BODY",
+  EMP = "EMP",
+}
+
+class Skill {
+  name: string;
+  stat: Stat;
+  lvl: number;
+  description: string | undefined;
+  x2: boolean;
+  
+  constructor( name: string, stat: Stat, x2: boolean = false, lvl: number | undefined = 0, description?: string | undefined ){
+    this.name = name;
+    this.stat = stat;
+    this.lvl = lvl;
+    this.x2 = x2;
+    this.description = description;
+  }
+  key(): string{
+    return this.name.toLocaleLowerCase().replace(/\W/g, '_');
+  }
+}
+
+
+const skill_list: [string, Stat, boolean?][] = [
+  ["Concentration", Stat.WILL ],
+  ["Conceal/Reveal Object", Stat.INT ],
+  ["Lip Reading", Stat.INT ],
+  ["Perception", Stat.INT ],
+  ["Tracking", Stat.INT ],
+  ["Athletics", Stat.DEX ],
+  ["Contortionist", Stat.DEX ],
+  ["Dance", Stat.DEX ],
+  ["Endurance", Stat.WILL ],
+  ["Resist Torture/Drugs", Stat.WILL ],
+  ["Stealth", Stat.DEX ],
+  ["Drive Land Vehicle", Stat.REF ],
+  ["Pilot Air Vehicle", Stat.REF, true ],
+  ["Pilot Sea Vehicle", Stat.REF ],
+  ["Riding", Stat.REF ],
+  ["Accounting", Stat.INT ],
+  ["Animal Handling", Stat.INT ],
+  ["Bureaucracy", Stat.INT ],
+  ["Business", Stat.INT ],
+  ["Composition", Stat.INT ],
+  ["Criminology", Stat.INT ],
+  ["Cryptography", Stat.INT ],
+  ["Deduction", Stat.INT ],
+  ["Education", Stat.INT ],
+  ["Gamble", Stat.INT ],
+  ["Language - Streetslang", Stat.INT ],
+  ["Library Search", Stat.INT ],
+  ["Local Expert", Stat.INT ],
+  ["Science", Stat.INT ],
+  ["Tactics", Stat.INT ],
+  ["Wilderness Survival", Stat.INT ],
+  ["Brawling", Stat.DEX ],
+  ["Evasion", Stat.DEX ],
+  ["Martial Arts", Stat.DEX, true ],
+  ["Melee Weapon", Stat.DEX ],
+  ["Acting", Stat.COOL ],
+  ["Play Instrument", Stat.TECH ],
+  ["Archery", Stat.REF ],
+  ["Autofire", Stat.REF, true ],
+  ["Handgun", Stat.REF ],
+  ["Heavy Weapons", Stat.REF, true ],
+  ["Shoulder Arms", Stat.REF ],
+  ["Bribery", Stat.COOL ],
+  ["Conversation", Stat.EMP ],
+  ["Human Perception", Stat.EMP ],
+  ["Interrogation", Stat.COOL ],
+  ["Persuasion", Stat.COOL ],
+  ["Personal Grooming", Stat.COOL ],
+  ["Streetwise", Stat.COOL ],
+  ["Trading", Stat.COOL ],
+  ["Wardrobe & Style", Stat.COOL ],
+  ["Air Vehicle Tech", Stat.TECH ],
+  ["Basic Tech", Stat.TECH ],
+  ["Cybertech", Stat.TECH ],
+  ["Demolitions", Stat.TECH, true ],
+  ["Electronics/Security Tech", Stat.TECH, true ],
+  ["First Aid", Stat.TECH ],
+  ["Forgery", Stat.TECH ],
+  ["Land Vehicle Tech", Stat.TECH ],
+  ["Paint/Draw/Sculpt", Stat.TECH ],
+  ["Paramedic", Stat.TECH, true ],
+  ["Photography/Film", Stat.TECH ],
+  ["Pick Lock", Stat.TECH ],
+  ["Pick Pocket", Stat.TECH ],
+  ["Sea Vehicle Tech", Stat.TECH ],
+  ["Weaponstech", Stat.TECH ],
+]
+
+enum CyberwareLocation {
+  Cyberears = "Cyberaudio Suite",
+  RCybereye = "Right Cybereye",
+  LCybereye = "Left Cybereye",
+  RCyberarm = "Right Cyberarm",
+  LCyberarm = "Left Cyberarm",
+  NeuralLink = "Neural Link",
+  RCyberleg = "Right Cyberleg",
+  LCyberleg = "Left Cyberleg",
+  Internal = "Internal",
+  External = "External",
+  Fashionware = "Fashionware",
+  Borgware = "Borgware"
+}
+
+class Weapon {
+  name!: string
+  dmg!: string
+  ammo: number | undefined
+  rof: number = 1
+  notes: string = ""
+  constructor(name: string, dmg: string = "1d6"){
+    this.name = name
+    this.dmg = dmg
+  }
+}
+
+class Character {
+  handle: string = "Unknown";
+  role: Role = Role.Civilian;
+  role_ability_rank: number = 0;
+  notes: string = ""
+  humanity: number = 0
+  stats: Record<string, number> = {}
+  skills: Record<string, Skill> = {}
+  critical_injuries: string[] = []
+  addictions: string[] = []
+  armor = {
+    head: { description: "", sp: 0, penalty: 0 },
+    body: { description: "", sp: 0, penalty: 0 },
+    shield: { description: "", sp: 0, penalty: 0 },
+  }
+  weapons: Weapon[] = []
+  gear: string[] = []
+  housing: string = ""
+  rent: number = 0
+  lifestyle: string = ""
+  fashion: string = ""
+
+  reputation: number = 0
+  reputation_events: string[] = []
+
+  cyberware: {
+    location: CyberwareLocation,
+    // foundational: boolean,
+    // requirement: string,
+    name: string,
+    data: string,
+    humanity_loss: number
+  }[] = []
+
+  lifepath: Lifepath | undefined
+
+  constructor(){
+    for( const stat of Object.values(Stat) )
+    {
+      this.stats[stat] = 0;
+    }
+    for( const skill of skill_list)
+    {
+      let name: string = skill[0];
+      let stat: Stat = skill[1];
+      let x2: boolean = skill[2]?true:false;
+      let new_skill = new Skill(name, stat, x2);
+      this.skills[new_skill.key()] = new_skill;
+    }
+  }
+  randomize(){  }
+}
+
+class Lifepath {
+  affectation = ""
+  cultural_origins = ""
+  personality = ""
+  clothing_style = ""
+  value_most = ""
+  feelings_about_people = ""
+  most_valued_person = ""
+  most_valued_possession = ""
+  family_background = ""
+  childhood_environment = ""
+  family_crisis = ""
+  life_goals = ""
+  friends: string[] = []
+  love_affairs: string[] = []
+  enemies: {
+    who: string, 
+    cause: string, 
+    what_can_they_throw: string, 
+    what_happens: string
+  }[] = []
+  role_specific: string[] = []
+}
+
+const char = ref(new Character())
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- {{ char }} -->
+    <div v-for="stat of Object.keys(char.stats)">
+      {{ stat }}: {{ char.stats[stat] }}
+    </div>
+    <hr />
+    <div v-for="skill of char.skills">
+      {{ skill.name }}: {{ skill.lvl }}
+    </div>
+
   </main>
 </template>
